c7ab04b7dabca9561f2fcae1392ab167a5170d07
Om Nimbalkar2004 <ompnimbalkar2004@gmail.com>
2024-07-31 19:10:23 +0530

    Small update

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,18 @@
-
+import { Box, Typography } from "@mui/material";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <Box>
+      <Typography
+        fontWeight="bold"
+        // color="#ffeba7"
+        color="#1DA1F2"
+        sx={{ mb: "30px", textAlign: "center", fontSize: "30px", top: "10%" }}
+      >
+        Coming Soon...
+      </Typography>
+    </Box>
+  );
+};
 
-export default SignIn
+export default SignIn;
